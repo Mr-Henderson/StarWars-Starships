2f3a87eddb2151c5f089e5b5efc11ab41cdb8ecd
Toby J. Henderson <bigtob@gmail.com>
2019-04-02 08:23:08 -0500

    Updates

---
+++ precache-manifest.bcac3b9d06c64ca105ad2946d1fd8226.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d86a4ed6272538da6b08a0a66ffdf70",
+    "url": "/StarWars-Starships/static/media/star3.4d86a4ed.jpg"
+  },
+  {
+    "revision": "b16e1446b87844dcea2984bbd8ce7cde",
+    "url": "/StarWars-Starships/static/media/star-wars-font.b16e1446.woff"
+  },
+  {
+    "revision": "4d77e52a71e26f0b2aae",
+    "url": "/StarWars-Starships/static/js/runtime~main.ec429aac.js"
+  },
+  {
+    "revision": "25885c2be8f5f0088547",
+    "url": "/StarWars-Starships/static/js/main.818ef4ee.chunk.js"
+  },
+  {
+    "revision": "c4c19265059000044f6e",
+    "url": "/StarWars-Starships/static/js/2.873ac3ed.chunk.js"
+  },
+  {
+    "revision": "25885c2be8f5f0088547",
+    "url": "/StarWars-Starships/static/css/main.13dca382.chunk.css"
+  },
+  {
+    "revision": "c4c19265059000044f6e",
+    "url": "/StarWars-Starships/static/css/2.02905e62.chunk.css"
+  },
+  {
+    "revision": "d51e75b36bac44c2479e61cf17843131",
+    "url": "/StarWars-Starships/index.html"
+  }
+];
